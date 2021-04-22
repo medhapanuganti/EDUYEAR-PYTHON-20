d16336c37dc8df738801856fa56394de11496cda
medhapanuganti <71941190+medhapanuganti@users.noreply.github.com>
2021-04-22 17:24:01 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,4 @@
+total_cases=65156
+recovered=51252
+deaths=20
+print("Total cases in hyderabad today are {}.\nRecovered cases are {}.\nNumber of deaths are {}.".format(total_cases,recovered,deaths))
