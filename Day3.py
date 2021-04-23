d1b5857d4c27a641b60a9d66e93a788bd03e0890
medhapanuganti <71941190+medhapanuganti@users.noreply.github.com>
2021-04-23 10:43:35 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,18 @@
+age=int(input("enter the age"))
+print("output age is",age)
+
+print("\nenter any two numbers")
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a%b=",a%b)
+print("a**b=",a**b)
+
+s=input("\nenter any string")
+print(s.count("y"))
+
+s1=input("\n enter any string")
+print(s1[0::2])
